6612229309410889406168c628016f3312c517d9
zNext666 <alonereview@gmail.com>
2020-03-18 01:44:57 +0700

    init tsx

---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import {NextPage} from 'next';
+
+const About: NextPage<{}> = () => (
+    <h1>About us</h1>
+);
+
+export default About;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+
+const Home: NextPage<{}> = () => (
+  <h1>Home</h1>
+);
+
+
+export default Home;
